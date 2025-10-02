2b145d6b43a62ab9e4f78ef14b59e3bff799311a
Regan Azfar <101255127+RnafZ@users.noreply.github.com>
2025-10-02 09:31:44 +0700

    The Sieve of Eratosthenes.c
    Cetak bilangan prima 2 - 997

--- The Sieve of Eratosthenes.c
+++ The Sieve of Eratosthenes.c
@@ -5,21 +5,25 @@ int main(){
     int prima[size];
     int i, j;
 
+    //set index ke 0 dan 1 menjadi 0
     prima[0] = 0;
     prima[1] = 0;
-    
+
+    //set semua elemen array menjadi 1, dimulai dari index ke 2
     for (i = 2; i < size; i++){
         prima[i] = 1;
     }
 
     for (i = 2; i < size; i++){
         if (prima[i] == 1){
+            //jika elemen array ke i bernilai 1, set setiap kelipatan dari i menjadi 0, yang artinya bukan prima
             for (j = i * 2; j < size; j += i){
                 prima[j] = 0;
             }
         }
     }
-    
+
+    //cetak semua iterasi jika array bernilai 1, yang artinya bilangan prima
     printf("Bilangan prima:\n");
     for (i = 2; i < size; i++){
         if (prima[i] == 1){
@@ -27,3 +31,4 @@ int main(){
         }
     }
 }
+
